7017ab21018eab0e9228f978a56e45e3228c8434
rolanos <ivankson@gmail.com>
2023-12-18 17:19:32 +0300

    fix

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -36,8 +36,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Widget> pages = [
     const Placeholder(),
+    NewsScreen(),
     const PlacesScreen(),
-    const SupportScreen(),
     const ProfilScreen(),
   ];
   @override
